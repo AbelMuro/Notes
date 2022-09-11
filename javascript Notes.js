c57c355a0b8b20d44657b5fe13e93cb240c56f64
AbelMuro <abelmuro93@gmail.com>
2022-09-11 12:17:43 -0700

    Update javascript Notes.js

--- javascript Notes.js
+++ javascript Notes.js
@@ -452,11 +452,25 @@ other_object.prototype.new_property = "whaterver";                          //pr
 
 
 
+//=========================================================== REG EXP ============================================================================
+//REGEXP are used to create a pattern that can be used to select certain parts of a string
 
+//syntax for regular expressions...  /pattern/modifiers
 
+//modifiers             /i   perform an case-insensitive matching
+//                      /g   perform a global match (doesnt stop at the first match)  
+//                      /m   perform a multi-line match
 
-
-
+//patterns
+let myString = "this is just an example for reg exp 1 2 3 4 5";
+myString.replace( / this /g , "");                       //will search for 'this' in the string
+myString.replace( / (this)|(is) /g , "");                //will search for 'this' and 'is' in the string
+myString.replace( / [1-4] /g, "");                       //will search for all numbers between 1 and 4 in the string
+myString.replace( / \d /g, "");                          //will search for a digit in the string
+myString.replace( / greetings\d /g , "");                //will search for 'greetings' and the first digit next to it
+myString.replace( / greetings\d+ /g, "");                //will search for 'greetings' and all the digits next to it 
+myString.replace( / \bLO /g, "");                        //will seach for a word that has 'LO' at the beginning of the word (will not select HELLO, but will select LOOK) 
+myString.replace( / \never\d+\.\d+ /g)                   //you can chain together reg exp, this will select 'never' then any digit, then a period, then another digit
 
 
 

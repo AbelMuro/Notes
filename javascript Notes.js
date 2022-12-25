a34f301ab6e5ce43f2907c1ce6b2cd88d207c75f
AbelMuro <abelmuro93@gmail.com>
2022-12-25 12:58:35 -0800

    Update javascript Notes.js

--- javascript Notes.js
+++ javascript Notes.js
@@ -230,10 +230,10 @@ function addNumbers(first = "9999523423423423435", second = "2762342342342342345
 / \v /	      //Find a vertical tab character
 / \n+ /           //Find a string that contains at least one n
 / \^n /           //Find a string that contains n at the beginning of it
-/ [^n] /         //find any character that is NOT n
+/ [^n]/         //find any character that is NOT n
 / \n$ /           //Find a string with n at the end of it
 / \a* /           //find zero or more of `a`
-
+/ . /             //will find all single characters from the string (its basically like splitting the string into an array)
 
 
 //its a good idea to use String.match() to use a reg exp to check if the pattern exists in the string

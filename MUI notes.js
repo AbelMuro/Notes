696f261cb1d02d8796718e4cdb5ee1ac85444019
AbelMuro <abelmuro93@gmail.com>
2023-01-23 17:47:30 -0800

    Update MUI notes.js

--- MUI notes.js
+++ MUI notes.js
@@ -1,7 +1,7 @@
 //how to set up MUI base in your application
 //
 //   1) npm install @mui/system  
-//      this package enables you to use styled()
+//      this package enables you to use styled(), which enables you to style MUI components
 //
 //   2) npm install @mui/icons-material
 //      this package lets you use all the icons that MUI has to offer
@@ -71,7 +71,7 @@ const StyledButton = styled(Button)`                      //styled is a function
 function MUI() {
             
        const handleClick() {
-       
+        alert("you clicked this button")
        }
 
       return(
@@ -138,15 +138,14 @@ function MediaQueries() {
 //------------------------------------------------------------ DIALOG POP UP BOXES ----------------------------------------------------------------
 import {Dialog, DialogContent, DialogTitle, DialogActions} from '@mui/material';
 
-
-
 function DialogBoxes(){
     const [openDialog, setOpenDialog] = useState(false);
             
     const handleDialog = () => {
-        setOpenDialog( (prevState) => {
-               return !prevState
-    })
+        setOpenDialog((prevState) => {
+               return !prevState;
+        })
+    }
            
    return (
        <Button onClick={handleDialog} variant="outlined">                                       //clicking this button will trigger the dialog box
@@ -158,11 +157,10 @@ function DialogBoxes(){
             <DialogContent> "This is where the content of the dialog is" </DialogContent>
             <DialogActions> 
                    <Button onClick={handleDialog}> "Cancel" </Button>
-                   <Button onClick={handleDialog}> "Subscribe" </Button>   
+                   <Button onClick={handleDialog}> "OK" </Button>   
             </DialogActions>
        </Dialog>
    )              
-  
 }
 
 
@@ -173,37 +171,76 @@ function DialogBoxes(){
     
     
     
- //----------------------------------------------------------- PAGINATION ---------------------------------------------------------------------
+ //----------------------------------------------------------- GRID WITH PAGINATION ---------------------------------------------------------------------
+//keep in mind that GRID was designed to handle very primitive data values, like strings and numbers.
+
 import {DataGrid} from '@mui/x-data-grid';                                //this package requires @mui/icons-material
 import {useDemoData} from '@mui/x-data-grid-generator';                   //generates fake data (make sure to npm install this too)
 
 export default function App() {
      const [pageSize, setPageSize] = useState(5);
+     const [rows, setRows] = useState([]);
+     const [columns; setColumns] = useState([]);
             
      const handleSizeChange = (newSize) => {                              //we create a re-render everytime the user decides to switch the number of items 
         setPageSize(newSize)
      };       
      
-     const {data} = useDemoData({                                         //generating fake data for the grid                 
-          dataSet: "Commodity",
-          rowLength: 100,
-          maxColumns: 6
-     });
-
+     useEffect(() => {                                                    //its a good idea to create the rows and columns of the grid
+            const currentRows = [
+                 { id: 1, col1: 'Hello', col2: 'World' },                 //this will define the actual data in the grid
+                 { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },      //remember to match these key names (col1, col2) to the key names (field) in the currentColumn
+                 { id: 3, col1: 'MUI', col2: 'is Amazing' },
+            ];
+           const currentColumns = [
+                 { field: 'col1', headerName: 'Column 1', width: 150 },   //this will define the column headers
+                 { field: 'col2', headerName: 'Column 2', width: 150 },
+            ];
+           setRows(currentRows);
+           setColumns(currentColumns);
+     },[])
+  
      return(
-       <div style={{height: 400, width: "100%"}}>                                   
+       <div style={{height: 400, width: "100%"}}>                       //its important to put the grid inside a container like this                        
           <DataGrid
-             pageSize={pageSize}                                         //the current page size (number of rows) in the grid
+             rows={rows}                                                 //actual data for the grid
+             columns={columns}                                           //column headers
+             pageSize={pageSize}                                         //the current page size (number of rows) in the first page of the grid
              onPageSizeChange={handleSizeChange}                        //event handler that will handle the change in page size
              rowsPerPageOptions={[5, 10, 20]}                           //the different number of rows that the user can choose between
-             pagination                                                 //keyword pagination is required
-             {...data}                                                  //actual data                             
+             pagination                                                 //keyword pagination is required                        
            />
         </div>
      );       
 }
 
 
+
+
+
+//-----------------------------------------------------------PAGINATION WITHOUT GRIDS ----------------------------------------------------------------------------
+import {Pagination} from '@mui/material';
+
+export default function BasicPagination() {
+     const [currentPage, setCurrentPage] = useState(1);               
+            
+     const handlePageChange = (event, newPage) => {
+           setCurrentPage(newPage)
+     }
+
+     return(
+           <>
+               <Pagination 
+                 page={currentPage}                         //current page
+                 onChange={handlePageChange}                //handler that will handle the page change
+                 count={10}                                 //number of pages
+                 color="secondary"                          //color of the selected page (in this case, the color will be purple)
+                 size="large"                               //size of all the text
+               />
+           </>
+     )
+
+}
 
 
 

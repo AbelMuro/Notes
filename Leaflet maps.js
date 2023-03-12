86c431738ff13467f2cdddedf58475b8b8cf5a8d
Abel Muro <abelmuro93@gmail.com>
2023-03-12 10:43:08 -0700

    Update Leaflet maps.js

--- Leaflet maps.js
+++ Leaflet maps.js
@@ -1,5 +1,19 @@
 //using leaflet interactive maps for react
 
+//remember to put the following lines in yout HTML file!
+/*
+    <link 
+        rel="stylesheet" 
+        href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+        integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+        crossorigin=""/>
+            
+    <script 
+        src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
+        integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
+        crossorigin="">
+    </script>
+*/
 
 import React, {useEffect, useState} from 'react';
 

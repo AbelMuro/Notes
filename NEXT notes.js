f81d40887b11ec5038ca151612e0ed371c561362
Abel Muro <abelmuro93@gmail.com>
2023-06-14 10:54:10 -0700

    Update NEXT notes.js

--- NEXT notes.js
+++ NEXT notes.js
@@ -2,17 +2,31 @@
     Next.js is a framework for React. A framework is a collection of pre-written JavaScript code libraries 
     that provide developers with pre-written JS code to use for routine programming features and tasks
 
-    Next.js has client-side routing, the pages folder automatically creates a URL for each js file
-    Next.js has pre-rendering (pre-fetching), Next.js will pre-render every page in the background, this results in faster routing
-                -the way this works is that NEXT.js will generate HTML for each page in advance, instead of the client side JS creating the HTML.
-                Then, the HTML will be 'hydrated'. This means that the JS code will run and make the page fully interactive
-    Next.js does code-splitting automatically, so each page only loads what’s necessary for that page 
-    Next.js has built in support for SASS
-    Next.js has image optimization, this means...
+    1) Next.js has client-side routing, the pages folder automatically creates a URL for each js file
+    
+    2) Next.js has pre-rendering, Next.js will pre-render every page in the background, this results in faster routing
+                -The way this works is that NEXT.js will generate HTML for each page in advance, instead of the client side JS creating the HTML.
+                    then, the HTML will be 'hydrated' once the HTML loads onto the browser. This means that the JS code will run and make the page fully interactive
+                -Next.js has two type of pre-rendering; Static Generation and server side rendering (the main difference is WHEN the html is generated)
+                    STATIC GENERATION is the pre-rendering method that generates the HTML at build time. The pre-rendered HTML is then reused on each request.
+                    SERVER-SIDE RENDERING is the pre-rendering method that generates the HTML on each page request. New HTML is generated for each page request
+                 -npm run dev will use static generation, but its possible to use a 'hybrid' app that uses static generation for some pages and server-side rendering for others
+                 -Next.js recommends that you use static generation whenever possible, but if the content of a page is constantly updating, then you should use server-side rendering
+                 -look for the sections static generation and server-side rendering for more info below
+                 
+    3) Next.js does code-splitting automatically, so each page only loads what’s necessary for that page 
+    
+    4) Next.js has built in support for SASS
+    
+    5) Next.js has image optimization, this means...
                 -the images are lazy loaded by default, 
                 -devs can use advanced configuration that includes props such as sizes, quality, loader and priority, etc...
                 -images are optimized on-demand as the users request them, instead of at build time, this improves build time
                 -Next.js can support modern image formats like WebP in supported browsers
+    
+    
+    
+    
     
 
     Steps for initializing Next.js
@@ -44,6 +58,149 @@
 
 
 
+
+
+
+
+
+
+
+
+
+//======================================================== _APP.js =========================================================================================
+// The _app.js file is a component that Next.js will use to pass EVERY page as props, 
+// this is useful for having global css or wrapping the app with a <Provider> from redux or Context
+
+import '../common/styles.css';
+
+export default function MyApp({Component, pageProps}) {         //Component is the page that is passed down to this component
+    return(                                                     //pageProps is the getStaticProps() or getServerSideProps() from the page
+        <Component {...pageProps} />        
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//======================================================== STATIC GENERATION WITH DATA =======================================================================
+// STATIC GENERATION is the pre-rendering method that generates the HTML at build time. The pre-rendered HTML is then reused on each request.
+// you can use getStaticProps() to make fetch requests for pages that are statically generated
+
+
+
+export default function Home(props) {           //this component will rely on the data from an API to populate its content when it first loads on the screen
+    const {userName, userEmail} = props;
+    
+    return (
+        <main>
+            {userName}
+            {userEmail}
+        </main>
+    )
+}
+
+export async function getStaticProps() {        
+  const data = await fetch('url');              //The value of the `props` key will be passed to the `Home` component
+
+  return data.json();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//=================================================== STATIC GENERATION WITHOUT DATA =========================================================================
+//Also known as Client-side rendering, for pages that dont need to be pre-rendered with data, 
+//you can use the fetch api or the SWR hook to fetch data at user request
+// client-side rendering should be used for specific pages where SEO is not relevant
+
+import useSWR from 'swr';
+
+export default Home () {
+    return (
+        <Profile/>
+    )
+}
+
+function Profile() {
+  const { data, error } = useSWR('/api/user', fetch);           //works similarly to fetch api
+
+  if (error) 
+      return <div>failed to load</div>;
+  if (!data) 
+      return <div>loading...</div>;
+  return <div>hello {data.name}!</div>;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//==================================================== SERVER-SIDE RENDERING WITH DATA ==============================================================================
+// SERVER-SIDE RENDERING is the pre-rendering method that generates the HTML on each page request. New HTML is generated for each page request
+// you can use getServerSideProps() to make fetch requests at page request
+// You should use getServerSideProps() only if you need to pre-render a page whose data must be fetched at page request time.
+
+export default function Home(props) {           //this component will rely on the data from an API to populate its content when it first loads on the screen
+    const {userName, userEmail} = props;
+    
+    return (
+        <main>
+            {userName}
+            {userEmail}
+        </main>
+    )
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      // props for your component
+    },
+  };
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 //============================================================ LINK COMPONENT ============================================================================================
 // Link component is used to navigate between the pages in a Next.js app
 
@@ -55,6 +212,17 @@ export default function FirstPost() {
         <Link href="/"> 'Back to home' </Link>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -93,6 +261,14 @@ export default function Home() {
 
 
 
+
+
+
+
+
+
+
+
 //======================================================== HEAD COMPONENT =========================================================================================================
 //Head component can be used to include meta data for the web app, such as the <title> and <link> tags
 
@@ -109,6 +285,13 @@ export default function Home() {
             </Head>
     )
 }
+
+
+
+
+
+
+
 
 
 
@@ -140,6 +323,19 @@ export default function Home() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 //========================================================== CSS in NEXT.js ==============================================================================================
 //You can include css modules in Next.js in the same way as React
 
@@ -162,6 +358,14 @@ export default function App({Component, props}) {                       //keep i
         <Component {...props} />        
     )
 }
+
+
+
+
+
+
+
+
 
 
 

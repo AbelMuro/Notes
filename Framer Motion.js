ac0025bf5882b1729e8734643f9df48884233ae7
Abel Muro <abelmuro93@gmail.com>
2023-07-31 20:56:02 -0700

    Update Framer Motion.js

--- Framer Motion.js
+++ Framer Motion.js
@@ -2,62 +2,113 @@
 //npm install framer-motion
 
 
-//-------------------------------- <motion/> component ---------------------------------------------------
-//in the example below, we use the motion component to create a div that will be styled into a circle
-//then we use the initial and animate props to animate the circle from invisible to visible
-// you can animate any property
+//==================================== <motion/> component =========================================================
+
 
 import {motion} from 'framer-motion';
 
+
+// INITIAL AND ANIMATE: this will animate the circle from opacity: 1 to opacity: 2
+// you can animate multiple properties at the same time
 function Circle () {
-  const constraints = useRef();
-  
   return(
     <> 
        <motion.div 
           className='circle' 
           initial={{opacity: 0, scale: 0.5}}               
-          animate={{opacity: 1, scale: 1.2}}              {/* this will animate the circle from opacity: 1 to opacity: 2 */}
-          transition={{duration: 0.5}}                    {/* it will take 0.5 seconds for the animation to complete */}
+          animate={{opacity: 1, scale: 1.2}}              
+          transition={{duration: 0.5}}                    
         />  
+  )
+}
 
+
+// ARRAYS: this will animate through the values of an array
+function Circle
+    return(
         <motion.div
-          animate={{                                      {/* You can animate through the values of an array */}       
+          animate={{                                      
             scale: [1, 2, 2, 1, 1],
             rotate: [0, 0, 270, 270, 0],
             borderRadius: ["20%", "20%", "50%", "50%", "20%"],
           }}
         />
+    )
+  }
+
+
           
+// HOVER AND TAB: this will create a hover animation and a click animation when the user hovers or clicks on the circle
+function Circle() {
+  return (
        <motion.div 
           className='circle'                    
-          whileHover={{scale: 1.2}}                        {/* this will create a hover animation for the circle*/}
-          whileTab={{scale: 0.9}}                          {/* this will create a click animation for the circle*/}
+          whileHover={{scale: 1.2}}                        
+          whileTab={{scale: 0.9}}                          
         />          
+    )
+}
 
+
+//DRAGGABLE: this will make the circle draggable but with certain constraints
+//the circle below can only be dragged 50px to the top, 50 px to the left, etc..
+function Circle() {
+    return(
         <motion.div    
-            drag                                           {/* this will make the circle draggable but with certain constraints*/}
+            drag                                         
             dragConstraints={{
-              top: -50,                                    {/* keep in mind that these 'contraints'*/}
+              top: -50,                                
               left: -50,
               right: 50,
               bottom: 50,
             }}
           />
-
-     <div>           
-        <motion.div 
-          className={'container'}                        {/* this div will be the container in which the draggable circle can be dragged*/}
-          ref={contraints}>                               {/* you must assign the same ref object to the container and the draggable item*/}
-
-        <motion.div    
-            drag                                           {/* this will make the circle draggable but with certain constraints*/}
-            dragConstraints={contraints}                   {/* if you assign a ref object to dragContraints*/}
-          />
-    </div>
-
-            
-    </>
-
-  )
+      )
 }
+
+
+//DRAGGABLE IN CONTAINER: you can create a large container and use it as the area in which a draggable item can be dragged
+function Circle() {
+      const contraints = useRef();
+  
+      return(
+         <div>           
+            <motion.div 
+              className={'container'}                        {/* large container that will be used for the draggable item*/}
+              ref={contraints}>                               {/* you must assign the same ref object to the container and the draggable item*/}
+
+            <motion.div    
+                drag                                           {/* draggable item*/}
+                dragConstraints={contraints}                  
+              />
+        </div>
+      )
+    
+}
+
+
+
+
+
+//================================== useMotionValue() and useTransform() =============================================================
+//These two hooks are used together to keep track of changes during an animation
+//useMotionValue returns a 
+
+import {motion, useMotionValue, useTransform } from 'framer-motion';
+
+function MotionValues() {
+    const x = useMotionValue(0);      
+    const background = useTransform(
+      x,
+      [-100, 0, 100],
+      ['red', 'green', 'white'],
+    )
+  
+    return(
+        <motion.div style={{background}}>
+        </motion.div>
+    )
+}
+
+
+

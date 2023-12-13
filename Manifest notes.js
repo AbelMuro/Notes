e35d9b6ef143b4cf77e9bc299f9e368e31fb7759
Abel Muro <abelmuro93@gmail.com>
2023-12-12 19:16:56 -0800

    Update Manifest notes.js

--- Manifest notes.js
+++ Manifest notes.js
@@ -2,30 +2,44 @@
 // The primary purpose of a manifest.json is to give the browser 
 // the necessary details to install a progressive web app (PWA) on a device
 // Remember to put the manifest.json file in the public folder of your app
+//Also make sure to use the CORRECT image sizes for the icons below
 
 //make sure to put this line of code in the index.html
-
 <link rel="manifest" href="/manifest.json"/>
 
 {
-    "short_name": "Quiz App",
-    "name": "Front end Quiz App",
+    "theme_color": "#FFFFFF",
+    "background_color": "#FFFFFF",
+    "display": "standalone",
+    "scope": "/",
+    "start_url": "/",
+    "name": "Password Generator App",
+    "short_name": "Generate Password",
+    "description": "App that will generate a password for you",
     "icons": [
       {
-        "src": "/quiz icon.png",                    //this image is used for splash screens
+        "src": "./icons/favicon-192x192.png",
+        "sizes": "192x192",
         "type": "image/png",
-        "sizes": "512x512",
-        "purpose": "any",
+        "purpose": "any maskable"
       },
-       {
-        "src": "/quiz icon.png",                   //this icon is used for your app in mobile devices (think of the youtube app on your phone, that app has an icon of a red box and a white play buttohn) 
+      {
+        "src": "./icons/favicon-256x256.png",
+        "sizes": "256x256",
         "type": "image/png",
-        "sizes": "196x196",
+        "purpose": "any maskable"
+      },
+      {
+        "src": "./icons/favicon-384x384.png",
+        "sizes": "384x384",
+        "type": "image/png",
+        "purpose": "any maskable"
+      },
+      {
+        "src": "./icons/favicon-512x512.png",
+        "sizes": "512x512",
+        "type": "image/png",
         "purpose": "any maskable"
       }
-    ],
-    "start_url": ".",
-    "display": "standalone",
-    "theme_color": "#000000",
-    "background_color": "#ffffff"
+    ]
   }

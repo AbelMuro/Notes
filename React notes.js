06a7e375f513db9b98cd278c04b579ad8afd5606
Abel Muro <abelmuro93@gmail.com>
2024-01-26 12:42:32 -0800

    Update React notes.js

--- React notes.js
+++ React notes.js
@@ -1151,8 +1151,8 @@ function expensiveCalculation() {
 // Re-styling a list of 1000 employee names is a low priority but heavy computation task
 // This hook will most likely be used in scenarios that involve search boxes and displaying results
 
-// The component below will have an input and will have a long list of employee names displayed below it
-// The component below basically has the logic that the <input/> updates are going to have priority over the <ListItems> update
+// The component below will have an input and will have a long list of movie names displayed below it
+// The component below basically has the logic that the search state updates are going to have priority over the results state updates
 
 import {useState, useTransition} from 'react';
 
@@ -1161,8 +1161,8 @@ function SearchBar() {
     const [results, setResults] = useState([])
     const [isPending, startTransition] = useTransition();
 
-    const handleQuery = (e) => {
-        setQuery(e.target.value);
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
         startTransition(() => {
             fetch('url')
                 .then(response => response.json())
@@ -1170,19 +1170,22 @@ function SearchBar() {
         })
     }
 
-
     return(
         <fieldset className={styles.inputContainer}>
             <input 
                 type='text' 
                 className={styles.searchBar} 
-                value={query}
-                onChange={handleQuery}
+                value={search}
+                onChange={handleSearch}
                 placeholder='Search for movies or TV series'
              />
-             {isPending && <span> 'Loading..' </span>}
-          -----------------------------------
-        </fieldset--------------------------------------------------
+             {isPending && <span> 'Loading..' </span>}                //fallback loading UI
+             {results.map((result) => {                               //state that will be updated when search state stops being updated
+                     return(
+                         //display results;
+                     )
+             })}
+        </fieldset
 
     )
 }
@@ -1198,30 +1201,43 @@ function SearchBar() {
 //   and useDeferredValue() will label a copy of the state as low priority
 
 
-//The component below has this logic, we have a state variable (query) with a copy of that state (deferredQuery)
+//The component below has this logic, we have a state variable (search) with a copy of that state (deferredSearch)
 //The whole component is a controlled component
 //When the user types a character in the <input/>, the update to the copy state will wait UNTIL the original state has
 // changed in the DOM
 //If the user continues typing in the <input/>, the update to the copy state will wait UNTIL the user stops typing
 // this is basically debouncing
+
 function App() {
-  const [query, setQuery] = useState('');                               //This state is high priority
-  const deferredQuery = useDefferedValue(query);                        //This state copy is low priority
+  const [search, setSearch] = useState('');                               //This state is high priority
+  const [results, setResults] = useState([]);
+  const deferredSearch = useDeferredValue(search, {timeoutMs: 1000});     //This state copy is low priority (the second argument can be used to delay the state update for 1 second)
 
-  const list = useMemo(() => {
-    return largeList.filter((item) => item.name.includes(query));
-  }, [deferredQuery]);                                                  //useMemo() will only change after the original state has finished updating in the dom
-
-  const handleChange = (event) => {
-    setQuery(event.target.value);
+  const handleSearch = (event) => {
+     setSearch(event.target.value);
   };
+        
+  useEffect(() => {
+    fetch(`https://example.com/api/search?q=${deferredSearch}`)
+      .then(response => response.json())
+      .then(results => setResults(results));
+  }, [deferredSearch]);                                             
+
 
   return (
     <>
-      <input type="text" value={query} onChange={handleChange} placeholder="Search" />
-      {list.map((item) => (
-        <SearchResultItem key={item.id} item={item} />
-      ))}
+      <input 
+          type="text" 
+          value={search} 
+          onChange={handleSearch} 
+          placeholder="Search" 
+        />
+        {results.map((result) => {
+                  return (
+                        //display results
+                  )
+        })}          
+      
     </>
   );
 }

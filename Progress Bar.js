482fe11ac89dc798c8f3045e3958b4574bee6b4b
Abel Muro <abelmuro93@gmail.com>
2024-03-06 19:26:59 -0800

    Create Progress Bar.js

---
+++ Progress Bar.js
@@ -0,0 +1,32 @@
+
+<progress 
+    value='45' 
+    max='100'>
+</progress>
+
+/* 
+      progress{
+          padding: 9px 11px;
+          color: var(--Dark-Navy, #261676);                  //actual bar will be colored dark navy
+          background-color: white;                          //background of container will be white
+          width: 240px;
+          height: 31px;
+          border-radius: 96px;
+      }
+      
+      progress::-moz-progress-bar {
+          border-radius: 96px;
+          background-color: var(--Dark-Navy, #261676);       //background of progress bar will be transparent
+      }
+      
+      progress::-webkit-progress-bar{
+          background-color: transparent;                      //background of progress bar will be transparent
+      }
+      
+      progress::-webkit-progress-value {
+          border-radius: 96px;
+          background-color: var(--Dark-Navy, #261676);       //actual bar will be colored dark navy
+      }
+
+
+*/

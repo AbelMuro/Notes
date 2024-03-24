8c3100e6eeed17337385d7d27d03f1325c76314d
Abel Muro <abelmuro93@gmail.com>
2024-03-23 17:58:42 -0700

    Update Framer Motion.js

--- Framer Motion.js
+++ Framer Motion.js
@@ -321,7 +321,7 @@ function App() {
         <AnimatePresence>
             {remove ? <></> : 
             <motion.div                      //before this element is removed, it will run some animation
-                key='1'                      //key prop is required for this to work (if its an img, then this should be assigned the src of the img as well)
+                key='1'                      //key prop is required for img tags
                 className={'box'} 
                 variants={variants} 
                 exit='exit'                  //the exit prop will use the exit property from the object in variants

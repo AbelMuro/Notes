37aca0b6c715969a45e0eed3742f09041ea9d16f
Abel Muro <abelmuro93@gmail.com>
2024-03-26 20:16:42 -0700

    Update React Native.js

--- React Native.js
+++ React Native.js
@@ -545,6 +545,9 @@ Platform
 //=================================================== REACT NATIVE SVG ===============================================
 //npm install react-native-svg
 //npm install react-native-svg-transformer -D
+//you may need to run the following commands if you are getting an error
+// cd ios
+// pod install
 
 import { SvgXml } from 'react-native-svg';
 

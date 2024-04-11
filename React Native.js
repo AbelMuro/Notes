fca9ecd372a0849a48812d5485d0ba6872ce1331
Abel Muro <abelmuro93@gmail.com>
2024-04-10 18:25:55 -0700

    Update React Native.js

--- React Native.js
+++ React Native.js
@@ -805,7 +805,29 @@ function PageTwo({text}){
 
 
 
+//================================================= REACT NATIVE DIALOG ============================================
+//npm install react-native-dialog
+import Dialog from "react-native-dialog";
 
+function App() {
+  const [text, setText] = useState('');
+
+  const handleText = (value) => {
+      setText(value);
+  }
+  
+  return(
+      <Dialog.Container visible={true}>
+          <Dialog.Title>'Account delete'</Dialog.Title>
+          <Dialog.Description>
+            'Do you want to delete this account? You cannot undo this action.'
+          </Dialog.Description>
+          <Dialog.Input value={text} onChange={handleText}/>            //all TextInput props will work with this component
+          <Dialog.Button label="Cancel" />
+          <Dialog.Button label="Delete" />
+      </Dialog.Container>
+  )
+}
 
 
 

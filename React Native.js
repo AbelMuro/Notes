3b0bd2a300d135962e7908ed470e44417537e871
Abel Muro <abelmuro93@gmail.com>
2024-04-15 22:44:04 -0700

    Update React Native.js

--- React Native.js
+++ React Native.js
@@ -919,6 +919,52 @@ export default App;
 
 
 
+//============================================ REACT NATIVE RADIO BUTTONS =========================================
+//npm install react-native-radio-buttons-group
+
+
+import RadioGroup from 'react-native-radio-buttons-group';
+
+function App() {
+    const [category, setCategory] = useState('Funny');          
+
+      const categories = [
+        {
+            id: 'Funny',                     //must be unique and a non-empty string            
+            label: 'Funny',
+            value: 'Funny',
+            borderColor: 'white',            //color for the border of the dot
+            color: 'white',                  //color of the dot in the middle
+            labelStyle: {color: 'white'}      // styles for the label
+        },
+        {
+            id: 'Music',
+            label: 'Music',
+            value: 'Music',
+        },
+        {
+            id: 'Sports',
+            label: 'Sports',
+            value: 'Sports',
+        }4
+    ]
+  
+    return(
+          <RadioGroup 
+              radioButtons={categories} 
+              onPress={setCategory}
+              selectedId={category}
+        /> )
+}
+
+
+
+
+
+
+
+
+
 
 //================================================= REACT NATIVE VIDEOS ===============================================
 //npm install react-native-video

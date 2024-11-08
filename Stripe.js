15208c0e3439452608da2a839436726d854144f2
Abel Muro <abelmuro93@gmail.com>
2024-11-08 13:28:25 -0800

    Update Stripe.js

--- Stripe.js
+++ Stripe.js
@@ -5,7 +5,15 @@
       0) In the stripe dashboard, you may need to create a 'store' that will keep track of the apps that use the stripe api 
           (go to the top left corner of the dashboard and select 'create new account')
 
-      1) brew install stripe/stripe-cli/stripe
+      1) brew install stripe/stripe-cli/stripe for mac
+
+         install the stripe_X.X.X_windows_x86_64.zip file from the stripe github repo for windows
+         unzip the file and save the file in a place you will remember 
+         then run the following commands in the command prompt
+         
+         cd path/to/directory/that/has/stripe.exe
+         
+         then run the commands below...
 
       2) stripe login                    //this will open up the browser, enabling you to login
 

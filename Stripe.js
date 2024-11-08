6d1d65df9f5f7c8800e9367a5acc1e35e65fb8fe
Abel Muro <abelmuro93@gmail.com>
2024-11-07 17:26:32 -0800

    Create Stripe.js

---
+++ Stripe.js
@@ -0,0 +1,18 @@
+/* 
+
+    Steps to install stripe into your app
+
+      0) In the stripe dashboard, you may need to create a 'store' that will keep track of the apps that use the stripe api 
+          (go to the top left corner of the dashboard and select 'create new account')
+
+      1) brew install stripe/stripe-cli/stripe
+
+      2) stripe login                    //this will open up the browser, enabling you to login
+
+      3) stripe plugin install apps      //you can create apps with this plugin
+
+      4) stripe apps create name-of-app  //this will create apps by opening up the browser and asking you to select a 'store' in stripe that you can use to keep the app
+
+
+        //https://docs.stripe.com/stripe-apps/create-app
+*/

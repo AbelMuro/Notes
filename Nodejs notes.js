7eb0853888616aa3cf160f70bfcb19bff1fd2a7f
Abel Muro <abelmuro93@gmail.com>
2024-12-21 23:40:47 -0800

    Update Nodejs notes.js

--- Nodejs notes.js
+++ Nodejs notes.js
@@ -159,9 +159,11 @@
 
 	     	const serverless = require('serverless-http'); 
 		const app = require('../src/index.js'); 		//make sure you export the app module from the index.js
+  		const connectDB = require('../src/Database/db.js');	//if you are using mongoose
 		
 		const handler = serverless(app);  		        //if you are NOT using mongoose, you can just return serverless(app);
-		module.exports.handler = async (e, context) => {	//if you are using mongoose, you will need to use a callback
+		module.exports.handler = async (e, context) => {	
+  		    await connectDB();					// you will need to call the connectDB() everytime there is a request made by the serverless function
 		    const result = await handler(e, context);
 		    return result;
 		};

b7faf1e0f29c9001c88113470b05ec85f923093d
Abel Muro <abelmuro93@gmail.com>
2025-03-22 16:03:48 -0700

    Create Google Cloud notes.js

---
+++ Google Cloud notes.js
@@ -0,0 +1,55 @@
+//==================================================== GOOGLE CLOUD ============================================================================
+
+/* 
+    Google cloud is a service that provides cloud computing. Google has a number of data centers around the globe, 
+    these data centers have computers, disk drives and virtual resources that can be utilized by other developers to 
+    build a website, software or server for their business or organization.
+
+    In other words, if a small business wants to deploy a server for their company, they can use google cloud
+    to host their servers, because google already has the hardware (in their data centers) required to maintain and contain servers
+    Developers dont need their own physical hardware or computer to host the server 24/7 when they have google cloud.
+
+*/
+
+
+
+
+
+//================================================= GOOGLE CLOUD RUN ===========================================================================
+/*
+    Google cloud run is a service that is used to deploy websites and servers.
+
+  
+    1) (FOR WINDOWS ONLY)  You will need to install the gcloud CLI on your computer first
+
+        https://cloud.google.com/sdk/docs/install
+
+        //then run the following command
+
+        gcloud init 
+
+    2) (FOR macOS ONLY) run the following commands to install the google cloud SDK
+          
+          brew install google-cloud-sdk          
+          gcloud init                          
+
+    3) If you already installed the google cloud CLI, then you can just run the following commands
+       Make sure you run the commands in the repository that you want to deploy    
+
+        gcloud auth login
+        gcloud config set project [PROJECT_ID]
+        gcloud run deploy            
+
+        //the last command will ask you a bunch of questions
+          
+    4) Go to your Google cloud console and then to Google Cloud Run
+  
+    
+
+*/ 
+
+
+
+
+  
+

4b2053bf1a260c3df7986e02eb63cbbd8cc99b37
Abel Muro <abelmuro93@gmail.com>
2025-03-22 17:55:38 -0700

    Update Google Cloud notes.js

--- Google Cloud notes.js
+++ Google Cloud notes.js
@@ -18,32 +18,19 @@
 //================================================= GOOGLE CLOUD RUN ===========================================================================
 /*
     Google cloud run is a service that is used to deploy websites and servers.
-
-  
-    1) (FOR WINDOWS ONLY)  You will need to install the gcloud CLI on your computer first
-
-        https://cloud.google.com/sdk/docs/install
-
-        //then run the following command
-
-        gcloud init 
-
-    2) (FOR macOS ONLY) run the following commands to install the google cloud SDK
           
-          brew install google-cloud-sdk          
-          gcloud init                          
+    1) Go to your Google cloud console and then to Google Cloud Run
 
-    3) If you already installed the google cloud CLI, then you can just run the following commands
-       Make sure you run the commands in the repository that you want to deploy    
+    2) Click on Connect Repo at the top left
 
-        gcloud auth login
-        gcloud config set project [PROJECT_ID]
-        gcloud run deploy            
+    3) Select github and click on Set up with Cloud Build
+        then select the repository you want to deploy
 
-        //the last command will ask you a bunch of questions
-          
-    4) Go to your Google cloud console and then to Google Cloud Run
-  
+    4) Click on Containers, Volumsn and Network Security
+        Select Variables & Secrets
+        Enter your env variables here
+    
+    5) Click on create
     
 
 */ 

a0c4197b662c92e1974f67242de85ac446ee2f61
Abel Muro <abelmuro93@gmail.com>
2025-03-22 20:10:02 -0700

    Update Docker-notes.js

--- Docker-notes.js
+++ Docker-notes.js
@@ -5,7 +5,7 @@
     Docker uses containers, they are an isolated environment that can be used to run any code
     We create a container by first creating an image, which is a blueprint on how the container will 'look like'
 
-    1) First download docker desktop (https://docs.docker.com/desktop/)
+    1) First download docker desktop (https://docs.docker.com/desktop/) and run the command wsl --install
 
     2) Then create a Dockerfile file in your root directory of your app (Dockerfile has no extention)
 

c615fd54297a1aa4833eaf4d20597c46347c03e2
Abel Muro <abelmuro93@gmail.com>
2025-03-22 21:00:29 -0700

    Update Google Cloud notes.js

--- Google Cloud notes.js
+++ Google Cloud notes.js
@@ -18,6 +18,10 @@
 //================================================= GOOGLE CLOUD RUN ===========================================================================
 /*
     Google cloud run is a service that is used to deploy websites and servers.
+
+
+
+
           
     1) Use Docker to create an image of your app (look at docker notes for more info)
 
@@ -35,7 +39,19 @@
     4) Run the following commands 
     
         docker tag name-of-image gcr.io/PROJECT_ID/name-of-image                    // you can get PROJECT_ID from the google console 
-        docker push name-of-image gcr.io/PROJECT_ID/name-of-image
+        docker push gcr.io/PROJECT_ID/name-of-image                                 // This will uploade the image to the Google Artifact registry
+
+        //if you have an issue with authorization, run the following command
+        gcloud auth configure-docker
+
+    5) Then go to 'Google Cloud Run' and then click on Deploy Container (click on 'service' in the dropdown)
+
+    6) Select Docker Hub and Artifact Registry
+       In the 'Container Image Url', click on select, and find the image that you uploaded on the Artifact Registry
+       In 'Containers, Volumes, Networking, Security', select 'Variables & Secrets' and put your env variables there
+       
+    7) Click on Create
+       
 */ 
 
 

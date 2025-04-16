8f01fbb3a87c4d7da25ded1dc5da4f35290dfcfa
Abel Muro <abelmuro93@gmail.com>
2025-04-15 17:32:07 -0700

    Update Vue.vue

--- Vue.vue
+++ Vue.vue
@@ -4,6 +4,32 @@
 
 
 
+
+    REACTIVITY SYSTEM:
+        -Deep Reactivity: all nested objects and arrays will be tracked within the state object
+        -Shallow Reactivity: Only a certain level of nested objects and arrays will be tracked within the state object
+
+        Every component in Vue.js will keep track of ALL of its state objects (ref)
+        When the state is updated, the component will be re-rendered.
+        
+        Vue uses objects as state, because Vue can intercept the get and set operations of an object 
+        with the setter and getter methods
+
+            const myRef = {
+              _value: 0,                   // the state value
+
+              get value() {
+                track()                    // when a value has been accessed by a component, we start the tracking process
+                return this._value
+              },
+
+              set value(newValue) {
+                this._value = newValue     // When the state changes, we cause a re-render
+                triggerRe-Render()
+              }
+            }
+
+
     Steps to initialize Vue.js
 
       1) npm init -y
@@ -64,57 +90,146 @@
 -->
 
 
-<!-- ============================================ VUE SYNTAX ======================================================= -->
+<!-- ============================================ SINGLE FILE COMPONENTS ======================================================= -->
+<!-- 
+    Single file components are files that contain one component (basically one template)
+    The template tag will automatically be exported from the file
+-->
 
-<!-- script tag allows you to import different modules from different files, you can also define the props -->
+<!-- script tag allows you to write JS code -->
 <script>
-  import HelloWorld from './components/HelloWorld.vue'     //assume that this component has the props 'msg'
-  defineProps({        
-    random: {
-      type: String,
-      required: true,
-    },
-    attributes: {                  
-      type: Object,
-      required: false
+    const x = 2 + "3";
+    const str = "random";
+    const myFunc = () => {
+        return "Hello-World"
     }
-  })
+
+    export default {                                                    //you can use the export default keyword and the setup function to use variables and functions within your template
+        setup(){
+            const otherFunc = () => console.log('Other function');
+            return {
+                x,
+                str,
+                myFunc,
+                otherFunc,
+            }
+        }
+    }
 </script>
 
 
 
 <!-- template tag allows you to use HTML syntax to declare and define your DOM elements (template tag will automatically be exported from this file)-->
 <template>
-    <HelloWorld msg="You did it!" />                                      <!-- You can pass props to a component like this --> 
-    <p class="myClass"> {{random === '' ? 'n' : 'v'}} </p>                <!-- {{}} allows you to use any JS expression in Vue-->
-
-      <!-- Directives-->
-    <div v-bind:class="'expression/variable goes here'"></div>          <!-- Attributes declared with v-bind: will make the attribute into a dynamic attribute, shorthand : -->
-    <div v-bind:[random === "" ? "id" : "class"]="url"></div>           <!-- You can dynamically assign a single attribute with the v-bind keyword -->
-    <div v-bind={{attributes}}></div>                                   <!-- You can dynamically assign multiple attributes with the v-bind keyword -->
-    <div v-if="'truthy or falsy expression/variable'"></div>            <!-- You can apply conditional rendering in Vue with the v-if directory-->
-    <button v-on:click="handleClick()"> Click here </button>            <!-- You can bind event handlers with the v-on directory, shorthand @ -->
+    <p class='myClass'> Hello World </p>                                      <!-- You can pass props to a component like this --> 
+    <button v-on:click="otherFunc()"> Click Here </button>                    <!-- Any variable or function exported using setup() will be available in the template-->
 </template>
 
 
 
-<!-- style tag allows you to use (remember to use scoped attribute to make sure the styles are scoped locally to a file)-->
+<!-- style tag allows you to use css (remember to use scoped attribute to make sure the styles are scoped locally to a file)-->
 <style scoped>            
-  header {
-    line-height: 1.5;
-  }
-
   .myClass {
     color: red;
   }
-
-  @media (min-width: 1024px) {
-    header {
-      display: flex;
-      place-items: center;
-      padding-right: calc(var(--section-gap) / 2);
-    }
-  }
 </style>
+
+
+
+
+
+
+
+
+<!--==================================================== TEMPLATES ======================================================== -->
+<!--
+    You can use the template tag to use html syntax to declare your elements within your single-file component
+-->
+
+<template>
+    <HelloWorld msg="You did it!" />                                      <!-- You can pass props to a component like this --> 
+    <p class="myClass"> {{random === '' ? 'n' : 'v'}} </p>                <!-- {{}} allows you to use any JS expression in Vue-->
+
+      <!-- Directives (anything inside the "" and [] must be an expression or variable that resolves to a string)-->
+    <div v-bind:class="dynamicValue"></div>                             <!-- Attributes declared with v-bind: will make the attribute into a dynamic attribute, shorthand : -->
+    <div v-bind:[dynamicAttribute]="url"></div>                         <!-- You can dynamically assign a single attribute with the v-bind keyword -->
+    <div v-bind={{attributes}}></div>                                   <!-- You can dynamically assign multiple attributes with the v-bind keyword -->
+    <div v-if="display"></div>                                          <!-- You can apply conditional rendering in Vue with the v-if directory (display must resolve to a truthy or falsey value)-->
+    <button v-on:click="handleClick()"> Click here </button>            <!-- You can bind event handlers with the v-on directory, shorthand @ -->
+    <form v-on:submit.prevent="handleSubmit()"></form>                  <!-- You can use modifiers to call certain functions for an event, prevent will call the e.preventDefault() for the submit event-->
+</template>
+
+
+
+
+
+
+
+
+
+<!-- ===================================================== STATE ============================================================ -->
+<!-- 
+    You can create a state object within your single-file component with the ref() function
+    You can also create a state object with the reactive() function
+
+        const countRef = ref(0);
+        const countReactive = reactive({count: 1, people: 23}); 
+
+    The difference is that the component will keep track of ALL the properties in countReactive, 
+    and the component will keep track of the 'value' property in countRef
+
+    Facts to keep in mind
+        -Do not pass down state as props directly
+        -All updates to the state are synchronous, but updates to the DOM are asynchronous
+         When you update multiple state objects, Vue will wait until all states have been updated,
+         then Vue will update the DOM by causing a re-render
+         If you want to change the state, update the dom, then change the state again synchronously, use the nextTick() function
+
+                async function incremenentThenDecrement() {
+                    count.value++;                     
+                    await nextTick();     //you can force a re-render here to update the DOM, instead of waiting for all states to be updated first
+                    count.value--;
+                }
+        
+
+-->
+
+<!-- All state objects and functions will be exported from the script when you use the setup attribute -->
+<script setup>                             
+    import {ref, reactive, nextTick} from 'vue';
+    
+    const count = ref(0);                  //Declaring state object with initial value of 0 
+    const state = reactive({ count: 0 })   //Declaring state object with initial value of {count: 0}
+    
+    function increment() {               
+        count.value++;                     //Every state object will have a value property, this is where the actual state is
+    }
+
+    function decrement() {
+        count.value--;                           
+    }
+    
+    console.log(count);             //{value: 0}
+    count.value++;                  //All updates to the state are synchronous
+    console.log(count);             //{value: 1}
+</script>
+
+
+<template>
+    <div> {{count}} </div>    <!-- You dont have to use the .value property of the state-->
+    <button v-on:click="increment()"> increase </button>
+</template>
+
+
+
+
+
+
+
+
+
+
+
+
 
 

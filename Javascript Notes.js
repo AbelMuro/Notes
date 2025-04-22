7debd9e177e05af96b90a18993af0238e2b01dfd
Abel Muro <abelmuro93@gmail.com>
2025-04-22 16:57:25 -0700

    Update Javascript Notes.js

--- Javascript Notes.js
+++ Javascript Notes.js
@@ -1,10 +1,58 @@
-//====================================================================DATA TYPES =================================================================================
-//javascript is a loosely typed language, meaning that the variables in JS are not bound to any particular type and can be assigned any type at any given time
-//javascript has garbage collectors, meaning that javascript will automatically free up space when a variable or object is not used anymore
-//javascript uses the stack and the heap to store data, the stack is used to store primitive values and references to objects, 
-      // and the heap is used to store the actual objects and data structures
-//also keep in mind, that in javascript, you dont need to put a semi-colon at the end of each line of code, javascript will do that for you
+/* 
+					             FEATURES OF JAVASCRIPT
 
+
+       							DYNAMICALLY TYPED
+	      	  Javascript is a dynamically typed language, meaning that any variable can change its data type at any time
+			     				let count = 4;
+				 			count = true;
+
+     							GARBAGE COLLECTORS
+	    	  Javascript will automatically free up space when a variable or object is not being used anymore
+
+  							  STACK/HEAP
+		  Javascript uses the stack and the heap to store temporary data. 
+    		      Stack: is used to store primitive values and references to objects, 
+      		      Heap: is used to store the objects and data structures
+
+       	           					EVENT PROPAGATION
+	           When an event is triggered in the DOM, JS will use Event Propagation.
+	           Lets say that we have a button nested inside a div, the button was clicked and triggered
+	           its on-click event. What happens next are the 3 main phases/steps of Event Propagation
+
+	                   1) Capturing Phase: JS will look for the button element that initially triggered the event.
+	                                       It starts at the top of the DOM tree (HTML) and works 
+	                                       its way down until it finds the button element. If there are any event listeners
+	                                       for the capturing phase in the parent elements, these elements will handle event
+	                                       before it reaches the button element
+	        
+	                                       div.addEventListener('click', () => {
+	                                           console.log('Div clicked during capturing phase');
+	                                        }, true);                             //third argument specifies that the event listener will be triggered on the capturing phase
+	        
+	                   2) Target Phase: Once JS finds the button element, it will trigger the on-click event handler
+	        
+	                   3) Bubbling Phase: The event will then 'bubble' up to the top of the DOM tree. Starting from the 
+	                                      button element, then finally to the html element. If any element (html, div) has an event listener for the 
+	                                      bubbling phase of the event, it will be triggered.    
+	        
+	                                      div.addEventListener('click', () => {
+	                                          console.log('Div clicked during bubbling phase');
+	                                        });
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+//====================================================================DATA TYPES =================================================================================
 //Expressions: are lines of code that produces a value
 "hello";                            //produces "hello"
 2 == 2;                             //produces true

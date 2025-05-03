bc9f05fe296992a985bf40fd29946aec3e93900b
Abel Muro <abelmuro93@gmail.com>
2025-05-03 00:01:45 -0700

    Update Javascript Notes.js

--- Javascript/Javascript Notes.js
+++ Javascript/Javascript Notes.js
@@ -29,6 +29,31 @@
 						     }
 						}
 
+      							       PROMISES
+		    	A promise is an object that represents the eventual completion or failure of a task. A promise is a way of telling us when
+       			an asynchronous operation has been completed. The promise will have one of three states; pending, fulfilled or rejected. 
+	  		Promises are the modern way of handling asynchronous operations. Before the invention of promises, asynchronous operations 
+     			were handled by callbacks. But it was easy to fall into the callback hell (unmanagable mess of code). Promises solve the problem 
+			of the callback hell. In javascript, we use the Promise constructor to create a promise object. These promise objects
+			have .then() and catch() methods that each handle the fulfilled and rejected states of the promise. Keep in mind, that the promise 
+   			constructor is synchronous, but the callback argument is asynchronous.
+
+					const promise = new Promise((resolve, reject) => {
+     						const success = true;
+    						if(success)
+     						    resolve('success');
+	    					else
+	   					    reject('rejected');
+					})
+     					promise
+	  				    .then((result) => {			//will be called if promise resolves	
+	  				       console.log(result);
+					    })
+     					    .catch((error) => {			//will be called if promise rejects
+	  					console.log(error);
+					    })
+ 
+
       							       CLOSURES
 		    	 A closure in Javascript is the concept that allows functions to use variables and objects that are declared 
 			 outside of its local scope. Variables being used inside a closure will not be garbage collected during the lifetime

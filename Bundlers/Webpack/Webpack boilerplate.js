08c41fdd976ecc118a9ff1a3fb8eaf8087bb7e77
Abel Muro <abelmuro93@gmail.com>
2025-06-01 09:17:43 -0700

    Create Webpack boilerplate.js

---
+++ Bundlers/Webpack/Webpack boilerplate.js
@@ -0,0 +1,44 @@
+const path = require('path');              
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const dotenv = require("dotenv-webpack")                     
+
+
+module.exports = {
+    entry: './src/index.js',                           
+    output: {                                             
+        path: path.join(__dirname, '/dist'),  
+        filename: 'bundle.js',                
+        publicPath: '/',
+    },
+    plugins: [                      
+        new HtmlWebpackPlugin({             
+            filename: 'index.html',           
+            template: './src/index.html'      
+        }),
+        new dotenv({systemvars: true}),       
+    ],
+    devServer: {                             
+        port: 3000,                           
+        historyApiFallback: true,            
+    },
+    
+    module: {
+        rules: [                               
+            {                                 
+                test: /\.js$/, 
+                use: {
+                    loader: 'babel-loader',  
+                    options: {presets: ['@babel/preset-env', '@babel/preset-react']}  
+                    }                                                                 
+            },
+            {
+                test: /\.css$/,
+                use: [{loader: 'style-loader'}, {loader: 'css-loader'}, {loader: 'postcss-loader'}]             
+            },
+            {
+                test: /\.(png|jpg|webp|mp4|wav|svg)$/,
+                type: 'asset/resource'                                              
+            }
+        ]
+    },
+}

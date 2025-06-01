d0e2f348f92da835ed58081a3d673eaadb1b6be6
Abel Muro <abelmuro93@gmail.com>
2025-06-01 09:25:13 -0700

    Update Babel-loader.js

--- Bundlers/Webpack/Modules/Babel-loader.js
+++ Bundlers/Webpack/Modules/Babel-loader.js
@@ -20,9 +20,18 @@ module.exports = {
               test: /\.js$/,                      
               use: {
                   loader: 'babel-loader',    
-                  options: {presets: ['@babel/preset-env', '@babel/preset-react']} 
+                  options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                  } 
               }                                                                
           },
     ]
   }
 }
+
+
+
+
+
+
+

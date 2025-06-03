a19431971cd025c657a4bcc96b6219cae63ca5ad
Abel Muro <abelmuro93@gmail.com>
2025-06-03 08:54:14 -0700

    Update Vue.vue

--- Vue/Vue.vue
+++ Vue/Vue.vue
@@ -19,45 +19,6 @@
                 14) Watchers (side-effects)
                 15) Computed (caching values)
                 16) Template Refs
-                
-
-
-                                                FEATURES OF VUE
-
-                                                BUNDLING PROCESS
-      When a developer finishes developing their Vue app, they will use a bundler (Vite)
-      to bundle all of the files into one file, bundle.js. 
-      
-        1) File Analysys 
-              The bundler will look for the entry point of the application (index.vue) and will analyze all 
-              imports and dependencies used in the application. The bundler will also check for syntax errors 
-      
-        2) Dependency Graph:
-              Then the bundler starts a dependency graph by linking all the modules and
-              assets together. 
-      
-        3) Transpilation: 
-                3.1)  Vue Templates:
-                      Once the dependency graph is completed, the bundler will transpile the templates into
-                      javascript render functions. T
-
-                3.2) Script Tags:
-                     Bundler will use esbuild to transpile any Javascript code in the script tags (Typescript, ES6+ syntax)
-
-                3.3) Style Tags
-                     The Style tags are checked to see if they need further transpilation (if using a pre-processor like SASS)
-                     The developer will need to install the necessary pre-processor compilers (PostCSS, node-sass, etc...)
-      
-        4) Optimization: 
-               The bundler will then implement tree-shaking, which is a process of removing any unused code
-               in the application. All whitespaces will be removed and variable names will be shortened to 
-               optimize the size of the file.
-
-        5) Bundling: 
-                Once the steps above have been completed, the bundle will bundle all the files into one 
-                file, bundle.js. If the developer implemented lazy-loading, the bundler will split the bundle.js
-                into multiple files, each file will be loaded on the browser when necessary
-
                             
                                                  STATE CHANGE PROCESS
         A component will be re-rendered (updated) when there is a change in the state object.

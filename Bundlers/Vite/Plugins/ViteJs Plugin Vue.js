b725b49151fa4645b073f9687a470d99e654ff57
Abel Muro <abelmuro93@gmail.com>
2025-06-10 19:13:50 -0700

    Create ViteJs Plugin Vue.js

---
+++ Bundlers/Vite/Plugins/ViteJs Plugin Vue.js
@@ -0,0 +1,11 @@
+/* 
+    The @vitejs/plugin-vue is an essential pluging that enables Vite to use Single file components in Vue
+*/
+
+import {defineConfig} from 'vite'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+})

7fb5c6c12c6ee074975143c4a53c2fc4db092132
Abel Muro <abelmuro93@gmail.com>
2025-06-10 19:18:40 -0700

    Update vite-plugin-vue-devtools.js

--- Bundlers/Vite/Plugins/vite-plugin-vue-devtools.js
+++ Bundlers/Vite/Plugins/vite-plugin-vue-devtools.js
@@ -1,6 +1,8 @@
 /* 
     The vite-plugin-vue-devtools plugin allows you to use DevTools to help you debug your vue application. 
     In the browser, press (F12) and then navigate to the Vue tab to inspect components, state and routes.
+
+    npm install vite-plugin-vue-devtools
 */
 
 import {defineConfig} from 'vue';

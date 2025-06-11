7ed567e66ac344127611c3f4dd6b30cf75d27ee5
Abel Muro <abelmuro93@gmail.com>
2025-06-10 19:19:05 -0700

    Update vitejs-plugin-vue.js

--- Bundlers/Vite/Plugins/vitejs-plugin-vue.js
+++ Bundlers/Vite/Plugins/vitejs-plugin-vue.js
@@ -1,5 +1,7 @@
 /* 
     The @vitejs/plugin-vue is an essential pluging that enables Vite to use Single file components in Vue
+
+    npm install @vitejs/plugin-vue
 */
 
 import {defineConfig} from 'vite'

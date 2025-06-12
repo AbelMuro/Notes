dc554cad2d8c64d4293697de11dd8460d56cfc2d
Abel Muro <abelmuro93@gmail.com>
2025-06-12 09:16:50 -0700

    Update MongoDB notes.js

--- NoSQL/MongoDB notes.js
+++ NoSQL/MongoDB notes.js
@@ -412,27 +412,7 @@
     The watch() method will return an 'change stream', this stream will 
     have its event handlers that will be triggered when something happens to 
     the collection (add new document, delete document, change document).
-
-
-    Syntax:
-
-        Queue.watch([
-            { 
-                $match: {                                                          // $match is used to specify a specific document in the collection
-                    'fullDocument.username' : 'abel muro',                         // we watch for documents that have the specified username (operation type can be any event EXCEPT delete)
-                    'documentKey._id': new ObjectId(),                             // we watch for a document that has the specified _id (operation type can be any event)
-                    'updateDescription.updatedFields.username': { $exists: true }  // if exists is true, then we only detect update events to the username property of a document; if exists is false, then we detect update events to documents where all properties, except username, were updated (operation type must be update)
-                    'ns.coll'
-                    'ns.db'
-                    'clusterTime'
-                    'operationType': { $in: ['insert', 'update', 'replace', 'delete'] }        // we detect the events 'insert', 'update', 'replace' on the document
-                }       
-            }
-        ], { fullDocument: 'updateLookup' })                                        // all update events will return the full document that was updated
-
-        
     
-
             Look at the Websocket module in Node.js notes for more info on how to integrate
             a mongoDB change-stream with a websocket.
         
@@ -451,27 +431,33 @@
             	    })
 */
 
-        const mongoose = require('mongoose');
-        const Queue = mongoose.model();
+       const mongoose = require('mongoose');
+       const Queue = mongoose.model();
+
+       const changeStream = Queue.watch([
+            { 
+                $match: {                                                          // $match is used to specify a specific document in the collection
+                    'fullDocument.username' : 'abel muro',                         // we watch for documents that have the specified username (operation type can be any event EXCEPT delete)
+                    'documentKey._id': new ObjectId(),                             // we watch for a document that has the specified _id (operation type can be any event INCLUDING delete)
+                    'updateDescription.updatedFields.username': { $exists: true }, // if exists is true, then we only detect update events to the username property of a document; if exists is false, then we detect update events to all properties EXCEPT username (operation type must be update)
+                    'ns.coll': { $in: ["collection1", "collection2"] } },          // we watch for events in different collections (even though Queue represents its own collection, you can reference other collection with this property)
+                    'ns.db': 'anotherDatabase',                                    // we watch for events in a different database (this should be used with ns.coll to specify the collection within this database)
+                    'operationType': { $in: ['insert', 'update', 'replace', 'delete'] }        // we detect the events 'insert', 'update', 'replace' on the document
+                } ,
+                startAtOperationTime: resumeTimestamp                               // this property ensures that the next event detected will wait after the specified timestamp (you can get an events timestamp in the changeStream event)
+            }
+        ], { fullDocument: 'updateLookup' })                                        // all update events will return the full document that was updated
 
 
 
 
 
-//---------------- Document Change Stream
 
-
-        const changeStream = Queue.watch([                                  // this will detect any changes made to a specific document in a collection
-                { 
-                    $match: { 
-                        'fullDocument.username' : username,                    // we detect changes made to a document that has the specified username
-                        operationType: { $in: ['insert', 'update', 'replace'] }}  //we detect the events 'insert', 'update', 'replace'
-                }
-            ], { fullDocument: 'updateLookup' });                             // all update events will return the full document 
-
+//---------------- Change Stream Event Handler
 
        changeStream.on('change', (change) => {                             // change event will be triggered when the document or collection is updated
-                const fullDocument = change.fullDocument;                       // contains the whole document that was updated or added to the collection
+                const fullDocument = change.fullDocument;                  // contains the whole document that was updated or added to the collection
+                const timeStamp = change.clusterTime;                      // clusterTime returns the timestamp of the event that was triggered
                 const operationType = change.operationType;         
                 /* 
                      possible values for change.operationType
@@ -485,36 +471,7 @@
                             dropDatabase – The database containing the collection was dropped.
                 */
         });
-
-
-
-
-
-
-//----------------Document Delete Stream
-
-        const deleteStream = Match.watch([                                // this will detect if the specified document was deleted
-            { 
-                $match: {
-                    'documentKey._id': new ObjectId(''),                      // we specify the document with its _id
-                     operationType: { $in: ['delete'] }                        // we detect the event 'delete'
-                }
-            }
-        ])
-
-
-        deleteStream.on('change', (e) => {
-            /* 
-                the delete event has the following properties
-                
-                    e.operationType = "delete"
-                    e.documentKey = { "_id": "..." }
-            */                      
-        })
-
-
-
-                                                                            
+                                                               
      
 //----------------Close Stream
 

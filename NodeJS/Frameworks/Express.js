9c5765a19e7285774de82d945f3df3c76713e597
Abel Muro <abelmuro93@gmail.com>
2025-06-17 05:58:52 -0700

    Update Express.js

--- NodeJS/Frameworks/Express.js
+++ NodeJS/Frameworks/Express.js
@@ -251,47 +251,6 @@
 
 
 
-//============================================================== CORS ======================================================================
-/* 
-	CORS: Cross origin resource sharing
-
- 	CORS is basically a way for servers to allow browsers of different origins to load resources
-   	If the browser is making a get-request from a different origin, the you will need cors
-
-    npm install cors
-*/
-    
-	
-
-    const cors = require('cors');
-    const corsOptions = {
-        origin: 'http://example.com',						    //Access-Control-Allow-Origin
-        methods: ['GET', 'POST'],                               //methods allowed
-        allowedHeaders: ['Content-Type', 'Authorization'],	    //Access-Control-Allow-Headers
-        credentials: true,                                      //allows cookies to be sent with the request/response
-        maxAge: 3600,       
-        optionsSuccessStatus: 200
-    }
-
-	app.use(cors(corsOptions));							        // Enabling cors for all routes using app.use()
-
-	app.get('/', cors(corsOptions), (req, res) => { 			// Alternatively, we can enable cors for specific routes using app.get(), app.post(), etc.
- 
-	});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 

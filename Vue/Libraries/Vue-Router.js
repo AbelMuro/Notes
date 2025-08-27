e00906fd5e7c4268352065cde965f064f0761fba
Abel Muro <abelmuro93@gmail.com>
2025-08-27 09:51:21 -0700

    Create Vue-Router.js

---
+++ Vue/Libraries/Vue-Router.js
@@ -0,0 +1,83 @@
+/* 
+      npm install vue-router@4
+
+      You can implement a multi-page application with Vue by using Vue-Router.
+*/
+
+
+
+
+
+
+
+
+
+
+//============================================== INITIALIZING VUE ROUTER ==============================================
+/* 
+    In your index.js file, add the following code..
+*/
+
+import {createWebHistory, createRouter} from 'vue-router';
+import Home from './Pages/Home';
+import AboutUs from './Pages/'
+
+const router = createRouter({
+    history: createWebHistory(),                        //this will save the URL endpoint of every route, and display it in the URL
+    routes: [
+        {path: '/', component: Home},
+        {path: '/aboutus', component: AboutUs}
+    ],
+})
+
+app.use(router);
+app.mount('#root');  
+
+
+/* 
+    In your App.vue file, you must use the <RouterView/> build-in component
+*/
+
+<template>
+    <RouterView/>              //this will display the current route component
+</template>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//============================================== NAVIGATION ==============================================
+
+
+//-------------------- useRouter()
+/* 
+    You can use the userRouter() hook to navigate to different components
+*/
+  
+<script setup>
+    import { useRouter } from 'vue-router'
+
+    const router = useRouter()
+
+    const handleOption = () => {
+        router.push('/checkerboard')
+    }
+
+</script>
+
+<template>
+      <button @click="handleOption">
+          Play Game
+      </button> 
+</template>
+

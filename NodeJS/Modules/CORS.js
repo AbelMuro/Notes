44b22d3851f65d62a72ead224790978c34ab1a8d
Abel Muro <abelmuro93@gmail.com>
2025-09-11 09:38:09 -0700

    Update CORS.js

--- NodeJS/Modules/CORS.js
+++ NodeJS/Modules/CORS.js
@@ -3,14 +3,31 @@
 	  CORS: Cross origin resource sharing
 
  	  CORS is basically a way for servers to allow browsers of different origins to load resources
-   	If the browser is making a get-request from a different origin, the you will need cors
+   	  If the browser is making a get-request from a different origin, then you will need cors
+
+	   	  NOTE TO SELF: There will be certain web hosts that use a strict comparison to compare the origins of the front end
+		  and the back end. 
+	   
+	   	  If the origin making a fetch request is 'https://world-class-checkers.netlify.app/' and the allowed origins
+		  for a node.js app is 'https://world-class-checkers.netlify.app'
+	
+	   					'https://world-class-checkers.netlify.app/' === 'https://world-class-checkers.netlify.app'
+	
+	  	   The expression above will return 'false' because of the trailing /
+		   Therefore, the origin at 'https://world-class-checkers.netlify.app/' will be invalid
 */
     
 	
 
     const cors = require('cors');
     const corsOptions = {
-        origin: 'http://example.com',						                //Access-Control-Allow-Origin
+        origin: 'http://example.com',						     //Access-Control-Allow-Origin
+		origin: (origin, callback) => {							   // the origin property can also accept a callback
+			if(allowedOrigins.includes(origin))					   // you can manipulate the origin URL string before comparing 
+				callback(null, true);							   // it to the allowed origin strings
+			else
+				callback(new Error('Not allowed by cors'))
+		}
         methods: ['GET', 'POST'],                               //methods allowed
         allowedHeaders: ['Content-Type', 'Authorization'],	    //Access-Control-Allow-Headers
         credentials: true,                                      //allows cookies to be sent with the request/response

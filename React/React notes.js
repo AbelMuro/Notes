d071612eed872da7a0f348d673fca996d19a6585
Abel Muro <abelmuro93@gmail.com>
2025-09-29 10:10:59 -0700

    Update React notes.js

--- React/React notes.js
+++ React/React notes.js
@@ -23,6 +23,16 @@
 
                                                              FEATURES OF REACT 
 
+                                                          RENDERING PROCESS (RECONCILIATION)
+                   Reconciliation is an algorithm that React uses to efficiently update the DOM. It starts by first mounting a component (and its element). 
+                   React will then create an Virtual DOM in memory. When a state change occurs, React will generate another Virtual DOM. At this point, 
+                   we have 2 Virtual DOMs in memory. React will use a process called 'diffing' to compare every node of both Virtual DOMs in memory,
+                   and will decide which nodes to update in the Real DOM. Once React has decided which nodes to change in the Real DOM, it will decide
+                   to either update, remove, or create nodes in the Real DOM. If a node needs to be updated, React will use a process called patching to 
+                   update the node. If a node needs to be removed or created, React will simply delete the node or create a new node in the Real DOM
+
+                                                             
+
                                                             STATE CHANGE PROCESS
                     A component will be re-rendered (updated) when there is a change in the state object.
                     All state updates behave asynchronously in react, and DOM updates also behave asynchronously.
@@ -42,13 +52,6 @@
                                                   setLoading(false)                // scheduling another state update
                                             }                                      // once we exit from the function, React will trigger the state update and re-render for setLoading(false) 
 
-                                                                RECONCILIATION
-                   Reconciliation is an algorithm that React uses to efficiently update the DOM. It starts by first mounting a component (and its element). 
-                   React will then create an Virtual DOM in memory. When a state change occurs, React will generate another Virtual DOM. At this point, 
-                   we have 2 Virtual DOMs in memory. React will use a process called 'diffing' to compare every node of both Virtual DOMs in memory,
-                   and will decide which nodes to update in the Real DOM. Once React has decided which nodes to change in the Real DOM, it will decide
-                   to either update, remove, or create nodes in the Real DOM. If a node needs to be updated, React will use a process called patching to 
-                   update the node. If a node needs to be removed or created, React will simply delete the node or create a new node in the Real DOM
 
                                                                 PATCHING
                    Patching is the process in React that updates nodes in the Real DOM when the state changes.

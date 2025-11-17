c5000832823ad7a68a408457f806b0b0d9aca69b
Abel Muro <abelmuro93@gmail.com>
2025-11-16 16:09:00 -0800

    Update Typescript.ts

--- Typescript/Typescript.ts
+++ Typescript/Typescript.ts
@@ -27,6 +27,52 @@
                   function getUser(user: User){}
 
 
+
+
+
+      HOW TO INTEGRATE TYPESCRIPT INTO A REACT APP
+
+            1) Create a tsconfig.json file and put it in the root directory
+
+                  {
+                    "compilerOptions": {
+                      "allowSyntheticDefaultImports": true,
+                      "outDir": "./dist/",
+                      "noImplicitAny": true,
+                      "module": "es6",
+                      "target": "es5",
+                      "jsx": "react",
+                      "allowJs": true,
+                      "moduleResolution": "node"
+                    },
+                      "include": ["src", "global.d.ts"]
+                  }
+      
+            2) Create a global.d.ts file and also put it in the root directory
+               The code below will enable the following syntax
+               import * as styles from './styles.module.css';
+
+                  declare module '*.module.css' {
+                        const classes: { [key: string]: string };
+                        export = classes;
+                  }
+
+            
+            3)  Then add the following lines of code to your webpack.config.js file
+                  module.exports = {
+                        module: {
+                            rules: [    
+                                {
+                                  test: /\.tsx?$/,
+                                  use: 'ts-loader',
+                                  exclude: /node_modules/,
+                                },                     
+                            ]
+                        },
+                        resolve: {
+                              extensions: ['.tsx', '.ts', '.js'],
+                        },
+                  }
 */
 
 

786878553c46ef8ab74374f1ff815ebba8b5437c
Abel Muro <abelmuro93@gmail.com>
2026-01-08 16:20:55 -0800

    Update Vue-Router.js

--- Vue/Libraries/Vue-Router.js
+++ Vue/Libraries/Vue-Router.js
@@ -39,104 +39,6 @@
 
 
 
-
-
-
-
-//============================================== NAVIGATION ==============================================
-
-
-      
-//-------------------- useRouter()
-/* 
-    You can use the userRouter() hook to navigate to different components.
-    This hook should only be used with the Composition API
-
-          const router = useRouter();
-
-          router.push()                        // push() will navigate to a different route
-          router.params.nameOfDynamicRoute     // params has access to the dynamic parameters of the URL
-*/
-  
-<script setup>
-    import { useRouter } from 'vue-router'
-
-    const router = useRouter()
-
-    const handleOption = () => {
-        router.push('/home');
-        router.push({      
-              name: 'home',                  //only works if the route in createRouter has the same name
-              params: {                      // the following properties will update the original URL
-                    id: ''
-              },
-              query: '',
-              hash: ''
-        })
-    }
-
-</script>
-
-<template>
-      <button @click="handleOption">
-          Play Game
-      </button> 
-</template>
-
-
-
-
-
-
-
-//-------------------- this.$router
-/* 
-      You can use this.$router object to nagivate to different routes.
-      This object should only be used with the Options API
-
-            
-*/
-
-<script>
-            export default {
-               methods: {
-                  handleNav() {
-                      this.$router.push('/about')
-                  },
-               },
-            }
-</script>
-
-
-
-
-
-//-------------------- <RouterLink/>
-/* 
-      You can use the RouterLink component to navigate to different routers.
-      This component can be used for both the Options API and the Composition API
-*/
-
-<template>
-      <RouterLink to="/">Go to Home</RouterLink>
-      <RouterLink to="/aboutus">About Us</RouterLink>
-</template>
-
-
-
-//-------------------- <RouterView/>
-/* 
-    You can use the <RouterView/> component to display the current route
-    This is a built-in component
-*/
-
-<template>
-    <RouterView/>            
-</template>
-
-
-
-
 //============================================== ROUTING ==============================================
 /* 
       In your index.js file, you can create different routes that map to specific components with
@@ -144,13 +46,22 @@
 */
 
 import Home from './Page/Home';
-import AboutUs from './Pages/AboutUs';
-
+import Account from './Pages/Account';
+import Settings from './Pages/Settings';
 
 const router = createRouter({
     routes: [
         {path: '/', component: Home},      
-        {path: '/aboutus', component: AboutUs}
+        {
+            path: '/account', 
+            component: Account,                  //Account component must have <RouterView/>
+            children: [
+                  {
+                     path: 'settings',
+                     component: Settings,
+                  }
+            ]
+        },
     ],
 })
 
@@ -198,6 +109,116 @@ const router = createRouter({
 
 
 
+
+
+
+
+
+
+      
+
+
+
+
+
+
+//============================================== NAVIGATION ==============================================
+
+
+
+//-------------------------------- Hooks -------------------------------- 
+      
+//-------------------- useRouter()
+/* 
+    You can use the userRouter() hook to navigate to different components.
+    This hook should only be used with the Composition API
+
+          const router = useRouter();
+
+          router.push()                        // push() will navigate to a different route
+          router.params.nameOfDynamicRoute     // params has access to the dynamic parameters of the URL
+*/
+  
+<script setup>
+    import { useRouter } from 'vue-router'
+
+    const router = useRouter()
+
+    const handleOption = () => {
+        router.push('/home');
+        router.push({      
+              name: 'home',                  // only works if the route in createRouter has the same name
+              params: {                      // the following properties will update the original URL
+                    id: ''
+              },
+              query: '',
+              hash: ''
+        })
+    }
+
+</script>
+
+<template>
+      <button @click="handleOption">
+          Play Game
+      </button> 
+</template>
+
+
+
+
+
+
+          
+//-------------------------------- Components -------------------------------- 
+
+          
+//-------------------- <RouterLink/>
+/* 
+      You can use the RouterLink component to navigate to different routers.
+      This component can be used for both the Options API and the Composition API
+*/
+
+<template>
+      <RouterLink to="/">Go to Home</RouterLink>
+      <RouterLink to="/aboutus">About Us</RouterLink>
+</template>
+
+
+
+//-------------------- <RouterView/>
+/* 
+    You can use the <RouterView/> component to display the current route
+    This is a built-in component
+*/
+
+<template>
+    <RouterView/>            
+</template>
+
+
+
+
+
+          
+          
+//-------------------------------- Miscellaneous -------------------------------- 
+          
+//-------------------- this.$router
+/* 
+      You can use this.$router object to nagivate to different routes.
+      This object should only be used with the Options API  
+*/
+
+<script>
+      export default {
+         methods: {
+            handleNav() {
+                this.$router.push('/about')
+            },
+         },
+      }
+</script>
 
 
 

0ff93ac946bdde8fb02e3543eac2477a1244c81c
Abel Muro <abelmuro93@gmail.com>
2026-05-29 15:22:50 -0700

    Create DOM Event System.js

---
+++ HTML/DOM Event System.js
@@ -0,0 +1,33 @@
+/*  
+                          DOM EVENT SYSTEM
+      Every browser has a native event system that uses events to cause a change in a web app.
+      This system uses the following three phases.
+
+*/
+
+/*
+            1. Capturing phase (phase 1)
+                  The event travels from the root (window/document) down the DOM tree toward the target.
+                  Listeners registered with addEventListener and {capture: true} fire here.
+*/
+                      document.addEventListener("click", () => {
+                      }, { capture: true });                      //this third argument ensures that the listener will be called during this phase
+
+
+
+/* 
+           2. Target phase (phase 2)
+                  The event reaches the target element.
+                  addEventListeners on the target fire (regardless of capture/bubble setting).
+*/
+                      document.addEventListener("click", () => {}); 
+
+
+/* 
+            3. Bubbling phase (phase 3)
+                  The event then travels back up the DOM tree toward the root.
+                  By default, all event listeners will fire during this phase, unless 
+                  {capture: true} is set on the third argument
+*/        
+
+                      document.addEventListener("click", () => {}); 

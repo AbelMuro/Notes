1174cfe71d47c6fa72f04c539d1aac821a61e728
Abel Muro <abelmuro93@gmail.com>
2026-05-31 16:27:20 -0700

    Create Variables.ts

---
+++ Typescript/Variables.ts
@@ -0,0 +1,8 @@
+//====================================== VARIABLES ======================================
+/*
+    The syntax for statically typing variables in TS is with the : operator
+*/
+
+const example : string;
+const example : number;
+const example : number | string;                  // Either a number or a string value can be used

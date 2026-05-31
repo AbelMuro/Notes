a80e2253be4d1c31bc7ad970fe91067817770029
Abel Muro <abelmuro93@gmail.com>
2026-05-31 16:28:49 -0700

    Add TypeScript array typing examples
    Added examples of statically typing arrays in TypeScript.

---
+++ Typescript/Arrays.ts
@@ -0,0 +1,9 @@
+// ===================================== ARRAYS =======================================
+/* 
+    You can use the type keyword to statically type an array
+*/
+
+let myArr : Array<string> = [];
+let myArr : Array<number> = [];
+let myArr : Array<[string, number]> = [];                  //will only accept an array with 2 elements of a string value and a number value
+let myArr : string[] = [];                                 // another way of statically typing an array

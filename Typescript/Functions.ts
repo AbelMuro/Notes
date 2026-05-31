4bb55fa6e35af964d538b35425cc37ef85656a6e
Abel Muro <abelmuro93@gmail.com>
2026-05-31 16:33:00 -0700

    Add myUser and getUser functions in Functions.ts
    Define myUser and getUser functions with string types.

---
+++ Typescript/Functions.ts
@@ -0,0 +1,15 @@
+// ===================================== FUNCTIONS =======================================
+/*
+    You can use a type to statically type the arguments and the return type of a function.
+*/
+
+
+function myUser(user: string){          //object passed as argument must be of type string
+      
+}   
+
+function getUser(): string {            //object returned must be of type string
+      
+}    
+
+const user = myUser('name of user');

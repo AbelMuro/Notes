0c018a1536b9d737d6cc3b16f302dee5637702ea
Abel Muro <abelmuro93@gmail.com>
2026-05-31 16:39:55 -0700

    Update Types-Interfaces.ts

--- Typescript/Types-Interfaces.ts
+++ Typescript/Types-Interfaces.ts
@@ -1,12 +1,24 @@
 /* 
                   TYPES and INTERFACES
                   
-      Type keyword can be used to create a custom data type for a variable, object, array or function
+      Both keywords are used to statically type a custom data type to a variables/array/function
+
+      Types should be used if you need better manipulation of the properties for the custom data type
+      you can better decide which properties can be included based on some condition
 */
 
 
 
 type customDataType = {
+      x: number,
+      y: string,
+      z?: string,                        //the question mark means that the property is optional
+      m: string | number,                //either string or a number value are acceptable
+      [key: string] : number,            //this syntax can represent any property name 
+      list: Array<string>,               //value must be an array of strings
+}
+
+interface customDataType = {
       x: number,
       y: string,
       z?: string,                        //the question mark means that the property is optional
@@ -50,131 +62,4 @@ type customDataType = {
 
 
 
-
-
-
-
-
-
-
-
-// ===================================== ARRAYS =======================================
-/* 
-    You can use the type keyword to statically type an array
-*/
-
-type Bookmark = {
-      name: string,
-      age: number
-}
-
-let myArr : Array<Bookmark> = [];
-let myArr : Array<string> = [];
-let myArr : Array<[string, number]> = [];                  //will only accept an array with 2 elements of a string value and a number value
-let myArr : string[] = [];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ===================================== OBJECTS =======================================
-/* 
-   You can use types to statically type an object. 
-   The object created from the interface must have the 
-   same property names and data types as the type
-*/
-
-type User {
-  name: string;
-  id: number;
-}
-
-const user: User = {
-  name: 'abel',        //must be 'name'
-  id: 123,              //must be 'id'
-  anyProperty: 2        //can be any and many properties
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ===================================== CLASSES =======================================
-/* 
-  You can use interfaces to statically type an object created from a class. 
-  The object created from the class must follow the rules of the interface
-*/
-
-type User {
-  name: string;
-  id: number;
-}
- 
-class UserAccount {
-  name: string;
-  id: number;
- 
-  constructor(name: string, id: number) {
-    this.name = name;
-    this.id = id;
-  }
-}
- 
-const user: User = new UserAccount("Murphy", 1);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ===================================== FUNCTIONS =======================================
-/*
-    You can use a type to statically type the arguments and the return type of a function.
-*/
-
-type User{
-    name: string,
-    id: number,
-}
-
-function myUser(user: User){          //object passed as argument must be of type User
-      
-}   
-
-function getUser(): User {            //object returned must be of type 'User'
-      
-}    
-
-myUser({                             //objects passed to the function above don't have to be declared with :User
-      name: 'abel',
-      id: 123
-});
 

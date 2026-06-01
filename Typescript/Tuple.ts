4ac237e602ad172373111fc2d502ffa114d601d0
Abel Muro <abelmuro93@gmail.com>
2026-06-01 16:41:52 -0700

    Create Tuple.ts

---
+++ Typescript/Tuple.ts
@@ -0,0 +1,8 @@
+/* 
+                TUPLE
+
+        You can use a tuple to statically type an array
+*/
+
+type x = [string, number]              // this data type will accept an array with a string 
+                                       // as the first value and a number as the second value

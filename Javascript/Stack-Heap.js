20713f2ca2b36e5562e7b00c8840f839dd2fe139
Abel Muro <abelmuro93@gmail.com>
2026-06-07 15:30:06 -0700

    Document stack and heap memory management in JS
    Added comments explaining the stack and heap in JavaScript.

---
+++ Javascript/Stack-Heap.js
@@ -0,0 +1,9 @@
+/* 
+  							                    STACK/HEAP
+                                    
+		         Javascript uses the stack and the heap to store and manage temporary data. The Stack is used for function calls, 
+	   		     every function call creates a stack frame that contain the execution context(local variables, other function calls) 
+       			 of the function, the stack frame is then placed into the stack. The heap is used to store objects and large data structures.
+			       The stack is faster, but has limited spaced. The heap is slower, but has dynamic memory allocation, in other words, it can
+    			   grow as needed.
+*/

255651cdedfafe7c73a802466549347762d7fae7
Abel Muro <abelmuro93@gmail.com>
2026-06-08 17:33:16 -0700

    Document Virtual DOM concept in Virtual DOM.js
    Add documentation for the Virtual DOM concept in React.

---
+++ React/Virtual DOM.js
@@ -0,0 +1,9 @@
+/* 
+                    VIRTUAL DOM
+
+      The virtual DOM is an exact copy of the REAL DOM, but it is used by React developers to 'mutate' the real DOM 
+      in the most efficient way possible. As React developers, we don't actually work with the Real DOM, we make changes
+      to the virtual DOM and React will efficiently update the real DOM using reconciliation
+        
+
+*/

61168388cfd61bbac877426cfa770c4ba30a8f4a
Abel Muro <abelmuro93@gmail.com>
2026-06-08 17:53:10 -0700

    Create Lifecycle.js with lifecycle documentation
    Added documentation on component lifecycle phases.

---
+++ React/Lifecycle.js
@@ -0,0 +1,16 @@
+/* 
+                                                                    LIFECYCLE
+                                                                    
+                    Lifecycle refers to the process that a component takes to be mounted onto the DOM and unmounted from the DOM.
+                    The lifecycle has 4 main phases; Initialization phase,  Mounting phase, Updating phase, and the Unmounting phase. 
+                    
+                      The initialization phase starts by creating the props, state, and functions of the component. 
+                    
+                      The mounting phase begins by rendering the component into the DOM for the first time. 
+                    
+                      The updating phase begins  when the state of the component is updated, thus triggering 
+                      a re-render(stuff gets updated on the screen) on the component.
+                    
+                      Then the umounting phase starts by removing the component (its JSX) from the DOM.
+
+*/

3608a318281ed10a00e859c4862f56d00cfe0ab4
Abel Muro <abelmuro93@gmail.com>
2026-06-11 18:28:34 -0700

    Create Component-Based Architecture.js

---
+++ React/Component-Based Architecture.js
@@ -0,0 +1,8 @@
+/* 
+                            COMPONENT-BASED ARCHITECTURE
+                            
+                  React uses a component-based architecture, where every part of the UI is broken down into components. Every component is separate 
+                  from other components, to the point that if a state change happens in one component, the other component will not be affected.
+                  Each component will be responsible for its part in the UI.
+
+*/

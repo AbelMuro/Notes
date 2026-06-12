8586bd911ed8f5b95615f9ef09f432efb5e5c1d1
Abel Muro <abelmuro93@gmail.com>
2026-06-11 18:27:11 -0700

    Create One-way Data binding.js

---
+++ React/One-way Data binding.js
@@ -0,0 +1,6 @@
+/* 
+                                                               ONE-WAY DATA BINDING
+                                                               
+                    All data in a React app flows in one direction. Typically, the parent component can pass its state down as props to the child component.
+*/
+

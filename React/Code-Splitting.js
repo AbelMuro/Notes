4414a4cd01418b4c272e8405168a8722abfc0a89
Abel Muro <abelmuro93@gmail.com>
2026-06-11 18:38:10 -0700

    Create Code-Splitting.js

---
+++ React/Code-Splitting.js
@@ -0,0 +1,7 @@
+/* 
+                                                                 CODE SPLITING
+                                                                 
+                    Instead of downloading the entire app before users can use it, code-splitting allows you to split your code into 
+                    small chunks which you can then load on demand. React achieves this by using the React.lazy() and React.suspense()
+                    Code Splitting is also the idea of breaking down the UI into different components
+*/

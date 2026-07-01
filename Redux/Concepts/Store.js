8f718ce215abb912475d77fbb14eb28a89b308e8
Abel Muro <abelmuro93@gmail.com>
2026-07-01 15:29:02 -0700

    Create Redux store configuration in Store.js
    Implement Redux store setup with root reducer and middleware.

---
+++ Redux/Concepts/Store.js
@@ -0,0 +1,30 @@
+/* 
+                                                                  STORE
+            The store in Redux is a centralized object that represents the single source of truth. The entire state object of the application
+            is stored here, and any components subscribed to the store will be re-rendered when the state is updated.
+
+*/
+
+
+// --------------------- store.js
+import {rootReducer} from './reducer/index.js';
+import {configureStore} from '@reduxjs/toolkit';
+
+const store = configureStore({              //creating a 'store' that will contain our state data
+    reducer: rootReducer,                   //our custom reducer
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}).concat(myMiddleware)  //adding a new middleware to our store
+}); 
+
+export default store;
+
+
+
+// ---------------------- index.js
+import {Provider} from 'react-redux';
+import store from './store.js';
+
+root.render(
+    <Provider store={store}>            {/*you must wrap your application with <provider store={store}> to enable the child components to dispatch actions and view state*/}
+        <App/>  
+    </Provider>
+)

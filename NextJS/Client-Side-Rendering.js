7f560b856b65ce3ffa30bc381bfc8bbdc1834b41
Abel Muro <abelmuro93@gmail.com>
2026-07-01 14:02:22 -0700

    Refine client-side rendering documentation
    Removed redundant steps in the client-side rendering explanation and clarified the process.

--- NextJS/Client-Side-Rendering.js
+++ NextJS/Client-Side-Rendering.js
@@ -3,10 +3,13 @@
     Next.js will use client side rendering by default, you can use React hooks and classes in client side rendering
                                                  
     Client side rendering is the process of rendering your application on your browser   
+
+    -Client-side rendering should be used for web apps that require high-interactivity, SEO doesn't matter, 
+    and the content always changes (Social media, chat apps, etc..)
     
-             1. First, we load the JavaScript to the client. When that has finished we can…                  
-             2. Fetch the data from the server. When that has finished we can….
-             3. We render the react components in the DOM… When that has finished we can…
+             1. First, we load the JavaScript to the client.                
+             2. Fetch the data from the server. 
+             3. We render the react components in the DOM
              4. We can use the application. — see the content on the page, click on things etc.
 */
 

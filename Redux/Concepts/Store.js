7d8e2a111a14326a58ae0e7a7ec74e33a47d2be0
Abel Muro <abelmuro93@gmail.com>
2026-07-01 15:33:41 -0700

    Enhance Store.js with state access and subscription details
    Added explanations and examples for accessing and subscribing to the Redux store state using useSelector and subscribe methods.

--- Redux/Concepts/Store.js
+++ Redux/Concepts/Store.js
@@ -30,6 +30,12 @@ root.render(
 )
 
 
+
+
+
+
+
+
 // ------------------------ accessing the state from the store
 /* 
             You can access the state from the store without subscribing 
@@ -45,3 +51,61 @@ const store = configureStore({reducer: myReducer});
 
 const state = store.getState();                                              //gets the complete state from the store
 const todos = store.getState(state => state.todos)                           //accessing a slice from the state
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------ Subscribing components to the store
+/* 
+            By subscribing components to the store, any state updates will re-render those components
+            You can use the following functions to subscribe.
+*/
+
+
+//------------------- useSelector() hook
+/* 
+            You can use the useSelector() hook to subscribe a component to the store.
+            It is also possible to subscribe to a very specific part of the store 
+            with this hook
+*/
+
+import {useSelector, shallowEqual} from 'react-redux';  
+import {createSelector} from '@reduxjs/toolkit';              
+
+const selectState = createSelector(                                //createSelector() can help you organize which parts of the state you want to subscribe
+    (state) => state.list,                 
+    (list) => list.names     
+
+)
+
+function App() {
+    const selectOne = useSelector(state => state.name)                // subscribing component to the 'name' property of the global state
+    const selectTwo = useSelector(state => state.list, shallowEqual)  // subscribing component to the first level of properties and values in the 'list' object
+    const selectThree = useSelector(selectState);                     // using a createSelector() to select a specific part of the state
+}
+
+
+
+
+//--------------------- .subscribe() method
+/* 
+            The store object that is returned from configureStore()
+            has the subscribe() method. You can use this method
+            to subscribe components to the store
+*/
+import {configureStore} from '@reduxjs/toolkit';
+
+const store = configureStore({reducer: myReducer});
+
+store.subscribe(Component)
+
+
+
+

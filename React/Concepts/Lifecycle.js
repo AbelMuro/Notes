de553c10190725fb105579e04a1ce00138ccaa79
Abel Muro <abelmuro93@gmail.com>
2026-07-01 12:44:59 -0700

    Update Lifecycle.js

--- React/Concepts/Lifecycle.js
+++ React/Concepts/Lifecycle.js
@@ -14,3 +14,81 @@
                       4) Then the umounting phase starts by removing the component (its JSX) from the DOM.
 
 */
+
+
+
+// ------------------------------------- CLASS LIFECYCLE METHODS -------------------------------------
+
+class ClassComponent extends React.Component {                    
+ 
+    static getDerivedStateFromProps(props, state){                                //lifecycle method that is called before the first render and after every render, and is used when the state is dependent on props
+        if(props.value !== state.value)
+              return {value : props.value};                                               //this will be the new state
+        else
+             return null;                                                                 //no changes are made to state
+    }
+        
+X   componentDidMount() {                                                         //lifecycle method that is called ONLY after the first render
+    }
+      
+    shouldComponentUpdate(nextProps, nextState){                                  //lifecycle method that determines if the component will update or not
+            if(this.props.val == nextProps.val && this.state.value == nextState.value)
+                  return false;                                                         //will NOT update
+            else
+                 return true                                                            //will update
+    }    
+        
+    getSnapShotBeforeUpdate(){                                                   //lifecycle method that is called before every render (but not before the first render)
+    }                                                                                     //anything that is returned from this function will be passed to compondentDidUpdate as an argument
+        
+        
+X   componentDidUpdate(prevProps, prevState, snapshot) {                         //lifecycle method that gets called after every render, except the first render
+    }
+
+X   componentWillUnmount() {                                                     //lifecycle method that will be called after the component has been removed from the DOM
+    }
+                                             
+}
+
+
+
+
+
+
+
+
+
+
+//------------------------------------- FUNCTION LIFECYCLES -------------------------------------
+
+// Mounting Phase
+
+useEffect(() => {          //getDerivedStateFromProps()
+},[props.value]);
+
+useEffect(() => {          //componentDidMount()
+},[]);
+
+
+
+//updating phase
+
+useEffect(() => {          //getDerivedStateFromProps()
+},[props.value]);
+
+memo();                    //shouldComponentUpdate()
+
+useEffect(() => {          //componentDidUpdate()
+});
+
+useCustomHook(() => {      // getSnapshotBeforeUpdate()                  
+})                         // you would have to implement your own custom hook to replicate this lifecycle method
+
+
+//unmounting phase
+
+useEffect(() => {
+    const unmount = () => {}; //function must be defined and declared inside the local scope of the useEffect
+    return unmount;       //componentWillUnmount
+})
+

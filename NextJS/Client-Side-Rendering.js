483213b2702e12e6b7fe091a598d08441d6a65dc
Abel Muro <abelmuro93@gmail.com>
2026-07-01 14:15:17 -0700

    Update Client-Side-Rendering.js

--- NextJS/Client-Side-Rendering.js
+++ NextJS/Client-Side-Rendering.js
@@ -7,9 +7,9 @@
     -Client-side rendering should be used for web apps that require high-interactivity, SEO doesn't matter, 
     and the content always changes (Social media, chat apps, etc..)
     
-             1. First, we load the JavaScript to the client.                
-             2. Fetch the data from the server. 
-             3. We render the react components in the DOM
+             1. The browser sends a request to the server             
+             2. The server sends a minimal HTML file with the javascript bundle
+             3. The browser will hydrate the page with javascript
              4. We can use the application. — see the content on the page, click on things etc.
 */
 

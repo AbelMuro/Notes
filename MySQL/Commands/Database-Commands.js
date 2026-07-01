245f47e4e0b47367a615b166a83d86aad000e2ee
Abel Muro <abelmuro93@gmail.com>
2026-07-01 15:06:10 -0700

    Create Database-Commands.js

---
+++ MySQL/Commands/Database-Commands.js
@@ -0,0 +1,13 @@
+//=============================== DATABASE COMMANDS
+
+//-------------- Creating a database
+CREATE DATABASE company;
+
+
+//-------------- Use a database to work with
+USE company
+
+
+//-------------- Delete a database
+DROP DATABASE company;
+

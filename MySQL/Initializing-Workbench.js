30f990028fe0e9949fd25dd829d562122e83c309
Abel Muro <abelmuro93@gmail.com>
2026-07-01 15:15:21 -0700

    Create Initializing-Workbench.js

---
+++ MySQL/Initializing-Workbench.js
@@ -0,0 +1,20 @@
+/*
+            -HOW TO CREATE A DATABASE IN MYSQL WORKBENCH (the workbench should be used development purposes only)
+
+                0) Keep in mind that Mysql must be running on the local machine.
+                    windows - open services.msc and start MYSQL80 
+                    iOS - open settings and start MYSQL
+
+                1) Click on the 'Create Schema' button on the top left corner
+                   and choose a name for the schema. Then click 'apply'
+
+                2) Then on the left hand side, right click on Tables and select 'Create Table'
+
+                3) You can create the columns of the Table. Each column will represent a piece of data.
+                   Each column must have a name, and a data type.
+
+                4) Then right click on the table and select 'Select Rows'
+
+                5) A list of all the data in the schema will be displayed
+
+  */

6eb265dee9d0861887488c98f91458d49e667da1
Abel Muro <abelmuro93@gmail.com>
2026-07-01 15:10:28 -0700

    Add update command for modifying a column value

---
+++ MySQL/Commands/Update-Commands.js
@@ -0,0 +1,4 @@
+//=============================== UPDATING ROW AND COLUMN COMMANDS
+
+//-------------- updating a column within a row
+UPDATE table_name SET column_three = 55000.00 WHERE column_one = 1;

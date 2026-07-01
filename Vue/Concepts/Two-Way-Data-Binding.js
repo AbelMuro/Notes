c81d439d38c9db5130eb46aecec06b15eb371188
Abel Muro <abelmuro93@gmail.com>
2026-07-01 12:20:14 -0700

    Create Two-Way-Data-Binding.js

---
+++ Vue/Concepts/Two-Way-Data-Binding.js
@@ -0,0 +1,8 @@
+/* 
+                                                  TWO WAY DATA BINDING
+        Vue primarily uses two way data binding with the 'v-model' directive and the defineModel() function. A parent component can pass 
+        down its state to the child component, and the child component can update the state directly with its own data. This is one way 
+        of passing data from the child component to the parent component. Keep in mind that the state used between the parent and child are 
+        synced. Any changes made to the state by the child component will reflect the state in the parent component, and vice versa
+
+*/

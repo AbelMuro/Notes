3b11cb2fdfc6dfb053d357340d3042de299274d2
Abel Muro <abelmuro93@gmail.com>
2026-07-01 12:19:19 -0700

    Create Virtual-DOM.js with Virtual DOM explanation
    Add documentation for the Virtual DOM concept in Vue.

---
+++ Vue/Concepts/Virtual-DOM.js
@@ -0,0 +1,6 @@
+/* 
+                                                    VIRTUAL DOM
+        Vue uses the virtual DOM, just like React. The Virtual DOM is an exact copy of the real DOM, and its used to determine which nodes 
+        in the real DOM have to be updated. When a component is first mounted, Vue will create a corresponding Virtual DOM node in the tree.
+
+*/

86414e3ec198179cfd418ec979299a229bd4f356
Abel Muro <abelmuro93@gmail.com>
2026-07-01 12:20:57 -0700

    Create Automatic-Batching.js

---
+++ Vue/Concepts/Automatic-Batching.js
@@ -0,0 +1,7 @@
+/* 
+                                                      Automatic- BATCHING
+          Vue has a similar batching process to the automatic batching used in React. When there are multiple state updates
+          that happen synchronously, Vue will 'batch' all these state updates into one asynchronous re-render. Keep in mind that
+          state updates that occur inside promises and setTimeout() will also be batched together.
+
+*/
